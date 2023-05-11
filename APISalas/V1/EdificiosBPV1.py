3b9aa81cb558ab321e50ac8147075fed1979a4e8
Ana Paula Barajas Campos <abarajas355@accitesz.com>
2023-05-11 15:14:40 -0600

    Servicio de Edificios

---
+++ APISalas/V1/EdificiosBPV1.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, request
+from V1.model import Conexion
+
+edificiosBP=Blueprint('EdificiosBP',__name__)
+
+#Registrar un edificio
+@edificiosBP.route('/Edificios/v1',methods=['POST'])
+def registrarEdificio():
+    data=request.get_json()
+    conexion=Conexion()
+    resp=conexion.registrarEdificio(data)
+    return resp
+
+#Consultar edificios
+@edificiosBP.route('/Edificios/v1',methods=['GET'])
+def consultarEdificios():
+    conexion=Conexion()
+    resp=conexion.consultarEdificios()
+    return resp
+
+#Consultar edificio por id
+@edificiosBP.route('/Edificios/v1/<id>',methods=['GET'])
+def consultarEdificioPorId(id):
+    conexion=Conexion()
+    resp=conexion.consultarEdificioPorId(id)
+    return resp
+
+#Modificar edificio
+@edificiosBP.route('/Edificios/v1',methods=['PUT'])
+def modificarEdificio():
+    data=request.get_json()
+    conexion=Conexion()
+    resp=conexion.modificarEdificio(data)
+    return resp
+
+#Dar de baja un edificio
+@edificiosBP.route('/Edificios/v1/<id>',methods=['PUT'])
+def darDeBajaEdificio(id):
+    conexion=Conexion()
+    resp=conexion.darDeBajaEdificio(id)
+    return resp
---
+++ APISalas/V1/model.py
@@ -0,0 +1,87 @@
+from pymongo import MongoClient
+from bson import ObjectId
+
+class Conexion():
+    def __init__(self):
+        self.cliente=MongoClient()
+        self.db=self.cliente.SalasREST
+
+    def registrarEdificio(self, data):
+        resp = {"estatus":"","mensaje":""}
+        edificio=self.db.Edificios.find_one({"nombreEdificio":data["nombreEdificio"]})
+        if edificio==None:
+            data["estatus"]="Activo"
+            self.db.Edificios.insert_one(data)
+            resp["estatus"]="OK"
+            resp["mensaje"]="Edificio registrado correctamente"
+        elif edificio["estatus"]=="Inactivo":
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="Este edificio ya existe pero está dado de baja"
+        else:
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="Este edificio ya existe, no se puede registrar"
+        return resp
+
+    def consultarEdificios(self):
+        resp={"estatus":"","mensaje":""}
+        edificios=self.db.Edificios.find({"estatus":"Activo"}) 
+        lista=[]
+        for s in edificios:
+            print(s)
+            lista.append({"_id":str(s["_id"]),"nombreEdificio":s["nombreEdificio"],"descripcion":s["descripcion"]})
+        if len(lista)>0:
+            resp["estatus"]="OK"
+            resp["mensaje"]="Lista de edificios"
+            resp["edificios"]=lista
+        else:
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="No hay edificios registrados"
+        return resp
+    
+    def consultarEdificioPorId(self, id):
+        resp={"estatus":"","mensaje":""}
+        edificio=self.db.Edificios.find_one({"_id":ObjectId(id)})
+        if edificio:
+            resp["estatus"]="OK"
+            resp["mensaje"]="Edificio encontrado"
+            resp["edificio"]={"_id":str(edificio["_id"]),"nombreEdificio":edificio["nombreEdificio"],"descripcion":edificio["descripcion"]}
+        else:
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="Edificio no encontrado"
+        return resp
+
+#Para modificar un edificio se debe validar que el edificio exista y que esté activo
+    def modificarEdificio(self, data):
+        resp={"estatus":"","mensaje":""}
+        edificio=self.db.Edificios.find_one({"_id":ObjectId(data["idEdificio"])})
+        if edificio:
+            if edificio["estatus"]=="Activo":
+                self.db.Edificios.update_one({"_id":ObjectId(data["idEdificio"])},{"$set":{"nombreEdificio":data["nombre"],"descripcion":data["descripcion"]}})
+                resp["estatus"]="OK"
+                resp["mensaje"]="Edificio modificado correctamente"
+            else:
+                resp["estatus"]="ERROR"
+                resp["mensaje"]="Este edificio está dado de baja, no se puede modificar"
+        else:
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="Edificio no encontrado"
+        return resp
+    
+    def darDeBajaEdificio(self, id):
+        resp={"estatus":"","mensaje":""}
+        edificio=self.db.Edificios.find_one({"_id":ObjectId(id)})
+        if edificio:
+            if edificio["estatus"]=="Activo":
+                self.db.Edificios.update_one({"_id":ObjectId(id)},{"$set":{"estatus":"Inactivo"}})
+                resp["estatus"]="OK"
+                resp["mensaje"]="Edificio dado de baja correctamente"
+            else:
+                resp["estatus"]="ERROR"
+                resp["mensaje"]="Este edificio ya está dado de baja"
+        else:
+            resp["estatus"]="ERROR"
+            resp["mensaje"]="Edificio no encontrado"
+        return resp
+
+    
+            
